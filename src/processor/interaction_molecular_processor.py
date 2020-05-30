1490932c4c2b89fba142bc6414df3a1935881a23
azurebrd <azurebrd@wobr2.caltech.edu>
2020-05-30 16:58:28 -0700

    AGR-2037 code structure for alliance headers, some values are placeholders

--- src/processor/interaction_molecular_processor.py
+++ src/processor/interaction_molecular_processor.py
@@ -5,13 +5,21 @@ import re
 import json
 import os
 import sys
-import strict_rfc3339
+# import strict_rfc3339
+import urllib.request
 import requests
 from tqdm import tqdm
+from datetime import datetime
+from string import Template
+
 
 from processor import Processor
 
 logger = logging.getLogger(__name__)
+
+
+class HeaderTemplate(Template):
+    delimiter = '%'
 
 
 class InteractionMolecularProcessor(Processor):
@@ -377,32 +385,63 @@ class InteractionMolecularProcessor(Processor):
             }
 
             # Write the comments in the main file.
-            the_time = strict_rfc3339.now_to_rfc3339_localoffset()
-            tsvout.writerow(['# Molecular Interactions'])
-            tsvout.writerow(['# Alliance of Genome Resources'])
-            tsvout.writerow(['# alliancegenome.org'])
-            tsvout.writerow(['# alliance-helpdesk@lists.stanford.edu'])
-            tsvout.writerow(['#'])
-            tsvout.writerow(['# PSI-MI TAB 2.7 Format'])
-            tsvout.writerow(['# Alliance Version {}'.format(self.context_info.env['ALLIANCE_RELEASE'])])
-            tsvout.writerow(['# Date Produced: {}'.format(the_time)])
-            tsvout.writerow(['#'])
-            # Write the headers
+            filetype = 'Molecular Interactions'
+            data_format = 'PSI-MI TAB 2.7 Format'
+            database_version = self.context_info.env["ALLIANCE_RELEASE"]
+            species = 'Many'
+            gen_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M")
+            readme = 'Readme placeholder'
+            response = urllib.request.urlopen(self.context_info.env["HEADER_TEMPLATE_URL"])
+            header_template = HeaderTemplate(response.read().decode('ascii'))
+            header_dict = {'filetype': filetype, 'data_format': data_format, 'stringency_filter': '',
+                           'taxon_ids': '', 'database_version': database_version, 'species': species,
+                           'gen_time': gen_time, 'readme': readme}
+            header = header_template.substitute(header_dict)
+            header_rows = [line.strip() for line in header.splitlines() if len(line.strip()) != 0]
+            for header_row in header_rows:
+                tsvout.writerow([header_row])
             tsvout.writerow(psi_mi_tab_header)
 
             for entry in out_write_list:
-                the_time = strict_rfc3339.now_to_rfc3339_localoffset()
-                entry.writerow(['# Molecular Interactions for {}'.format(out_to_species_name_dict[entry])])
-                entry.writerow(['# Alliance of Genome Resources'])
-                entry.writerow(['# alliancegenome.org'])
-                entry.writerow(['# alliance-helpdesk@lists.stanford.edu'])
-                entry.writerow(['#'])
-                entry.writerow(['# PSI-MI TAB 2.7 Format'])
-                entry.writerow(['# Alliance Version {}'.format(self.context_info.env['ALLIANCE_RELEASE'])])
-                entry.writerow(['# Date Produced: {}'.format(the_time)])
-                entry.writerow(['#'])
-                # Write the headers
+                filetype = 'Molecular Interactions for {}'.format(out_to_species_name_dict[entry])
+                species = out_to_species_name_dict[entry]
+                header_dict = {'filetype': filetype, 'data_format': data_format, 'stringency_filter': '',
+                               'taxon_ids': '', 'database_version': database_version, 'species': species,
+                               'gen_time': gen_time, 'readme': readme}
+                header = header_template.substitute(header_dict)
+                header_rows = [line.strip() for line in header.splitlines() if len(line.strip()) != 0]
+                for header_row in header_rows:
+                    entry.writerow([header_row])
                 entry.writerow(psi_mi_tab_header)
+
+#           Old format for headers
+# 
+#           the_time = strict_rfc3339.now_to_rfc3339_localoffset()
+#           tsvout.writerow(['# Molecular Interactions'])
+#           tsvout.writerow(['# Alliance of Genome Resources'])
+#           tsvout.writerow(['# alliancegenome.org'])
+#           tsvout.writerow(['# alliance-helpdesk@lists.stanford.edu'])
+#           tsvout.writerow(['#'])
+#           tsvout.writerow(['# PSI-MI TAB 2.7 Format'])
+#           tsvout.writerow(['# Alliance Version {}'.format(self.context_info.env['ALLIANCE_RELEASE'])])
+#           tsvout.writerow(['# Date Produced: {}'.format(the_time)])
+#           tsvout.writerow(['#'])
+#           # Write the headers
+#           tsvout.writerow(psi_mi_tab_header)
+#
+#           for entry in out_write_list:
+#               the_time = strict_rfc3339.now_to_rfc3339_localoffset()
+#               entry.writerow(['# Molecular Interactions for {}'.format(out_to_species_name_dict[entry])])
+#               entry.writerow(['# Alliance of Genome Resources'])
+#               entry.writerow(['# alliancegenome.org'])
+#               entry.writerow(['# alliance-helpdesk@lists.stanford.edu'])
+#               entry.writerow(['#'])
+#               entry.writerow(['# PSI-MI TAB 2.7 Format'])
+#               entry.writerow(['# Alliance Version {}'.format(self.context_info.env['ALLIANCE_RELEASE'])])
+#               entry.writerow(['# Date Produced: {}'.format(the_time)])
+#               entry.writerow(['#'])
+#               # Write the headers
+#               entry.writerow(psi_mi_tab_header)
 
             psi_mi_tab_header.insert(0,'Reason for skipping row.')
             skipped_out.writerow(psi_mi_tab_header)
